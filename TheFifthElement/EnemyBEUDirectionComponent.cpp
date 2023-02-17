ed63abb7135b739e647bb74d9189d1ca850d6d83
sosanc03 <99793450+sosanc03@users.noreply.github.com>
2023-02-17 12:51:08 +0100

    comentarios eliminados
    Se me había olvidado boorar unos comentarios que tenia como referenci para el movimiento del enemigo Beat Em' Up

--- TheFifthElement/EnemyBEUDirectionComponent.cpp
+++ TheFifthElement/EnemyBEUDirectionComponent.cpp
@@ -39,34 +39,3 @@ void EnemyBEUDirectionComponent::changeDir(Vector2D d)
 {
 	dir_ = d;
 }
-
-
-/*void EnemyBEUDirectionComponent::handleEvents()
-{
-
-	InputHandler::instance()->update(event);
-
-	if (InputHandler::instance()->keyDownEvent())
-	{
-		if (InputHandler::instance()->isKeyDown(SDL_SCANCODE_A)) {
-			mov_->setDir(Vector2D(-1, 0));
-		}
-
-		else if (InputHandler::instance()->isKeyDown(SDL_SCANCODE_D)) {
-			mov_->setDir(Vector2D(1, 0));
-		}
-
-		else  if (InputHandler::instance()->isKeyDown(SDL_SCANCODE_W)) {
-			mov_->setDir(Vector2D(0, -1));
-		}
-		else if (InputHandler::instance()->isKeyDown(SDL_SCANCODE_S)) {
-			mov_->setDir(Vector2D(0, 1));
-		}
-		else if (InputHandler::instance()->isKeyDown(SDL_SCANCODE_R)) {
-			std::cout << "ATTACK" << endl;
-		}
-		else mov_->setDir(Vector2D(0, 0));
-	}
-
-
-}*/
