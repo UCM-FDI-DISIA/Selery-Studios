57470a9110efea5d6b4b644fca1d38bbdba2b80d
sheilajulvez <sjulvez@ucm.es>
2023-02-26 14:17:58 +0100

    Nuevas layers y ampliacion

---
+++ TheFifthElement/assets/MapAssets/AncientRuins/TiledMap Editor/PlataformaA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="PlataformaA" tilewidth="32" tileheight="32" tilecount="108" columns="12">
+ <image source="../Props/generic_estructure2-on flat surface-all details.png" width="403" height="311"/>
+</tileset>
