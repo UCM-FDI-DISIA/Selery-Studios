08b370966c32139267bd645fa6422c3e40849bc1
PabloM <pabmar31@ucm.es>
2023-03-04 00:04:55 +0100

    Quitado LimitBEU en salto
    Ahora se puede saltar en todas partes

--- TheFifthElement/src/components/LimitBEU.cpp
+++ TheFifthElement/src/components/LimitBEU.cpp
@@ -1,16 +1,16 @@
 #include "LimitBEU.h"
 
 LimitBEU::LimitBEU() {
-
 }
 
 void LimitBEU::initComponent() {
 	tr_ = ent_->getComponent<Transform>(TRANSFORM_H);
+	jmp_ = ent_->getComponent<JumpComponent>(JUMP_H);
 	downLimit = sdlutils().height() - 100 - tr_->getH();
 	topLimit = sdlutils().height() / 4;
 }
 
 void LimitBEU::update() {
-	if (tr_->getPos().getY() < topLimit) { tr_->setPos(Vector2D(tr_->getPos().getX(), topLimit)); }
+	if (tr_->getPos().getY() < topLimit && jmp_->isJumpEnabled()) { tr_->setPos(Vector2D(tr_->getPos().getX(), topLimit)); }
 	else if (tr_->getPos().getY() > downLimit) { tr_->setPos(Vector2D(tr_->getPos().getX(), downLimit)); }
 }
--- TheFifthElement/src/components/LimitBEU.h
+++ TheFifthElement/src/components/LimitBEU.h
@@ -2,11 +2,14 @@
 #include "../utils/Entity.h"
 #include "../utils/Component.h"
 #include "Transform.h"
+#include "JumpComponent.h"
 
 class LimitBEU : public Component{
 private:
     Transform* tr_ = nullptr;
+    JumpComponent* jmp_ = nullptr;
     float downLimit, topLimit;
+
 public:
     LimitBEU();
     void initComponent();
