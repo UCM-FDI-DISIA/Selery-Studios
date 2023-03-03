9bc0e5b0f849290660a54b7a70d6e42dde2bbd91
JoseMorenob <100274773+JoseMorenob@users.noreply.github.com>
2023-03-04 00:30:37 +0100

    Union con main

--- TheFifthElement/src/Entities/EnemyBEU.cpp
+++ TheFifthElement/src/Entities/EnemyBEU.cpp
@@ -22,10 +22,10 @@ EnemyBEU::EnemyBEU(Vector2D pos,PlayerBEU* player, float maxLife, string enemy, 
 	anim_->updateAnimation();
 
 
-	tr_ = addComponent<Transform>(int(TRANSFORM_H), pos, EnemyWidth_, EnemyHeight_, EnemyRotation_, speed_, nframes_, matrix_);
-	trPlayer_ = player_->getComponent<Transform>(int(TRANSFORM_H));
+	//tr_ = addComponent<Transform>(int(TRANSFORM_H), pos, EnemyWidth_, EnemyHeight_, EnemyRotation_, speed_, nframes_, matrix_);
+	//trPlayer_ = player_->getComponent<Transform>(int(TRANSFORM_H));
 	//tr_ = addComponent<Transform>(TRANSFORM_H, EnemyPosition_, EnemyWidth_, EnemyHeight_, EnemyRotation_, speed_, nframes_, matrix_);
-	tr_ = addComponent<Transform>(TRANSFORM_H, EnemyPosition_, EnemyWidthQUESISIRVE_, EnemyHeightQUESISIRVE_, EnemyRotation_, speed_, nframes_, matrix_);
+	tr_ = addComponent<Transform>(TRANSFORM_H, pos, EnemyWidthQUESISIRVE_, EnemyHeightQUESISIRVE_, EnemyRotation_, speed_, nframes_, matrix_);
 	trPlayer_ = player_->getComponent<Transform>(TRANSFORM_H);
 
 
