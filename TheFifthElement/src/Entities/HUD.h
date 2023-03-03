a3c86224ba012b213be913dab36f4a0ac317bf3a
Cao33 <pablocao@ucm.es>
2023-03-04 00:02:36 +0100

    creacion hud como entity

---
+++ TheFifthElement/src/Entities/HUD.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "../components/Image.h"
+#include "../components/InputComponent.h"
+#include "../utils/ecs.h"
+#include "../sdlutils/Texture.h"
+#include "../utils/Entity.h"
+
+class HUD
+{
+};
+
