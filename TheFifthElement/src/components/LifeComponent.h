684d6942713285379c2774dd818cce28d369be05
sosanc03 <99793450+sosanc03@users.noreply.github.com>
2023-03-04 22:08:15 +0100

    fix
    Hay una barra de vida sobre el enemigo BEU que está comentado, funciona simplemente descomentándolo.

--- TheFifthElement/src/components/LifeComponent.h
+++ TheFifthElement/src/components/LifeComponent.h
@@ -14,26 +14,5 @@ public:
 	void subLife(float damage);
 	void resetLife() { life_ = maxLife_; }
 	void render();
-	//void render() {
-	//	SDL_Rect dest;
-	//	dest.x = trEntity->getPos().getX();
-	//	dest.y = trEntity->getPos().getY();
-	//	dest.h = 20;
-	//	dest.w = 100;
-	//	SDL_Rect src;
-	//	src.x = 0;
-	//	src.y = 0;
-	//	src.h = trEntity->getH();
-	//	src.w = trEntity->getW();
-	//	//lifeBar->render(trEntity->getPos().getX(), trEntity->getPos().getY() - 50);
-	//	lifeBarBack->render(src, dest);
-	//	lifeBarBorder->render(src, dest);
-	//	SDL_Rect destL;
-	//	destL.x = trEntity->getPos().getX();
-	//	destL.y = trEntity->getPos().getY();
-	//	destL.h = 20;
-	//	destL.w = life_;
-	//	lifeBar->render(src, destL);
-	//}
 };
 
