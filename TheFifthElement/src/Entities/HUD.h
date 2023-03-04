b0062b2b8ed09d0b94f0f6d6cc46067bca94cd51
Cao33 <pablocao@ucm.es>
2023-03-04 18:45:30 +0100

    Logica de ruleta de personajes escrita

--- TheFifthElement/src/Entities/HUD.h
+++ TheFifthElement/src/Entities/HUD.h
@@ -10,6 +10,11 @@ class HUD:public Entity
 {
 private:
 	string skins_[4] = { "air","fire","water","earth" };
+	Vector2D frontIcon_ = { 50,50 };
+	Vector2D backIcon_ = { 50,50 };
+	Vector2D leftIcon_ = { 50,50 };
+	Vector2D rightIcon_ = { 50,50 };
+	Vector2D iconPos_[4] = {frontIcon_,rightIcon_,backIcon_,leftIcon_}; //va a seer innecesario
 public:
 	//debería tener una constructora
 	HUD();
