5a18211cfef073be508948af963a76964d9185a0
PabloCerrada <75524849+PabloCerrada@users.noreply.github.com>
2023-03-09 22:15:36 +0100

    Reino de fuego (2 / x)

---
+++ TheFifthElement/assets/Scenes/Maps/Tileset-TerrainGrassLand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset-TerrainGrassLand" tilewidth="32" tileheight="32" tilecount="2538" columns="54">
+ <image source="GrassLand/Tilesets/Tileset-Terrain.png" width="1759" height="1504"/>
+</tileset>
