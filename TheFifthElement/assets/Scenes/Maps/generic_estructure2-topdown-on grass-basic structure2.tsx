6021e423429a0f27419ae97d7e60b13603f09173
sheilajulvez <sjulvez@ucm.es>
2023-03-12 13:39:05 +0100

    mas y mas mapa

---
+++ TheFifthElement/assets/Scenes/Maps/generic_estructure2-topdown-on grass-basic structure2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="generic_estructure2-topdown-on grass-basic structure2" tilewidth="32" tileheight="32" tilecount="121" columns="11">
+ <image source="AncientRuins/Props/generic_estructure2-topdown-on grass-basic structure2.png" width="382" height="382"/>
+</tileset>
