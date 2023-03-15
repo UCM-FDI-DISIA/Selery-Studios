bcc55ed22551e82c8e329e4e7d7dd47251ad0955
PabloCerrada <75524849+PabloCerrada@users.noreply.github.com>
2023-03-15 13:41:23 +0100

    Jefe de tierra (2 / x)
    Animation Component

---
+++ TheFifthElement/AnimationEarthBossComponent.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef ANIMATIONEARTHBOSSCOMPONENT_H_
+#define ANIMATIONBOSSCOMPONENT_H_
+#include "src/utils/Vector2D.h"
+#include "src/utils/Component.h"
+#include "src/utils/Entity.h"
+#include "src/components/Image.h"
+
+class AnimationEarthBossComponent : public Component
+{
+private:
+	Image* imBoss;
+public:
+	enum Animations {
+		Idle, Moving, Attack, Hit, Death, IdleProtection, Protection
+	};
+	AnimationEarthBossComponent() {};
+	virtual ~AnimationEarthBossComponent() {}
+	void initComponent();
+	void newAnimation(Animations newAnim);
+};
+#endif
+
+
--- TheFifthElement/LifeEarthBossComponent.h
+++ TheFifthElement/LifeEarthBossComponent.h
@@ -4,15 +4,17 @@
 #include "src/utils/Vector2D.h"
 #include "src/utils/Component.h"
 #include "src/utils/Entity.h"
-
+#include "AnimationEarthBossComponent.h"
 class LifeEarthBossComponent : public Component
 {
 private:
+	AnimationEarthBossComponent* animEarthBoss;
 	int life, maxLife;
 public:
 	LifeEarthBossComponent();
 	virtual ~LifeEarthBossComponent() {}
 	void receiveDamage(float damage, float mul);
+	void initComponent();
 };
 #endif
 
