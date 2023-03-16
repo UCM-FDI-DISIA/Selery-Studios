6c9b9315b798d945f63edcf2f88e46d48767d95f
JoseMorenob <100274773+JoseMorenob@users.noreply.github.com>
2023-03-16 11:51:40 +0100

    SOMBRITA HECHA

--- TheFifthElement/src/components/ShadowComponent.cpp
+++ TheFifthElement/src/components/ShadowComponent.cpp
@@ -33,11 +33,18 @@ void ShadowComponent::render() {
 	else {
 		SDL_Rect rect;
 		rect.x = trans_player->getPos().getX() - mngr_->camRect_.x + trans_player->getW() / 2
-			- (trans_player->getW() / 2 * trans_player->getS()) / 2
-			-((trans_player->getW() / 2 * trans_player->getS()) * (jump_poss_y - trans_player->getPos().getY()) / 100);
+			- (trans_player->getW()/2 * trans_player->getS())/2 
+			+ (10 + (trans_player->getW() / 2 * trans_player->getS()) * (1 / abs(jump_poss_y - trans_player->getPos().getY())))+src_width/2;
 		rect.y = jump_poss_y - mngr_->camRect_.y + trans_player->getH() - src_width / 2;
 		rect.h = src_width;
-		rect.w = ((trans_player->getW() / 2 * trans_player->getS())*(jump_poss_y-trans_player->getPos().getY())/100);
+		
+		rect.w = (10+(trans_player->getW() / 2 * trans_player->getS())*(1/abs(jump_poss_y-trans_player->getPos().getY())));
+		if (rect.w < 20) {
+			rect.w = 20;
+			rect.x = trans_player->getPos().getX() - mngr_->camRect_.x + trans_player->getW() / 2
+				- (trans_player->getW() / 2 * trans_player->getS()) / 2
+			+rect.w+src_width/2-5;
+		}
 		SDL_Rect src;
 		src.x = 0;
 		src.y = 0;
