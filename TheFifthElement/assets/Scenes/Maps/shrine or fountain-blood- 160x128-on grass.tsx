3d5bf976b01d2461c5fc5c0100384198db146a61
sheilajulvez <sjulvez@ucm.es>
2023-04-18 11:53:23 +0200

    Revert "Revert "Merge branch 'Sheila_Julvez'""
    This reverts commit 0189c0a217bc5e0ef7945d3792ceb67cbd5e7b6b.

--- TheFifthElement/assets/Scenes/Maps/shrine or fountain-blood- 160x128-on grass.tsx
+++ TheFifthElement/assets/Scenes/Maps/shrine or fountain-blood- 160x128-on grass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="shrine or fountain-blood- 160x128-on grass" tilewidth="32" tileheight="32" tilecount="160" columns="40">
+<tileset version="1.9" tiledversion="1.9.2" name="shrine or fountain-blood- 160x128-on grass" tilewidth="16" tileheight="16" tilecount="640" columns="80">
  <image source="AncientRuins/Props/shrine or fountain-blood- 160x128-on grass.png" width="1280" height="128"/>
 </tileset>
