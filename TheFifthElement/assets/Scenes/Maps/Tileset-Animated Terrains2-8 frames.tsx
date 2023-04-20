93665a04ac9b8d39b579b2b8d0329ac654f24258
PabloCerrada <75524849+PabloCerrada@users.noreply.github.com>
2023-04-20 17:05:07 +0200

    Terreno del reino de fuego (casi me da algo)

---
+++ TheFifthElement/assets/Scenes/Maps/Tileset-Animated Terrains2-8 frames.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset-Animated Terrains2-8 frames" tilewidth="16" tileheight="16" tilecount="2088" columns="58">
+ <image source="AncientRuins/Tilesets/Tileset-Animated Terrains2-8 frames.png" width="928" height="576"/>
+</tileset>
