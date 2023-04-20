64a1cd866000bcf8257abae7cdc21cfe7f83170c
sheilajulvez <sjulvez@ucm.es>
2023-04-20 18:44:48 +0200

    mucho avance mapa aire

---
+++ TheFifthElement/assets/Scenes/Maps/shrine or fountain 160x128-on flat surface.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="shrine or fountain 160x128-on flat surface" tilewidth="16" tileheight="16" tilecount="640" columns="80">
+ <image source="AncientRuins/Props/shrine or fountain 160x128-on flat surface.png" width="1280" height="128"/>
+</tileset>
