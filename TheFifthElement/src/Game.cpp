042ee07d6103802f3cd34b0c961ebe22c72e3943
sosanc03 <99793450+sosanc03@users.noreply.github.com>
2023-05-02 16:17:22 +0200

    escala de menú de pausa

--- TheFifthElement/src/Game.cpp
+++ TheFifthElement/src/Game.cpp
@@ -10,13 +10,14 @@ Game::Game() {
 
 	//GameStateMachine::instance()->pushState(new MainMenuState());
 	//GameStateMachine::instance()->pushState(new TopDownState());
-	GameStateMachine::instance()->pushState(new BeatEmUpState(false, nullptr));
+	//GameStateMachine::instance()->pushState(new BeatEmUpState(false, nullptr));
 	//GameStateMachine::instance()->pushState(new BeatEmUpState(true, nullptr, "earth"));
 	//GameStateMachine::instance()->pushState(new BeatEmUpState(true, nullptr, "water"));
 	//GameStateMachine::instance()->pushState(new BeatEmUpState(true, nullptr, "light"));
 	//GameStateMachine::instance()->pushState(new BeatEmUpState(true, nullptr, "fire"));
 	//GameStateMachine::instance()->pushState(new PauseState());
 	//GameStateMachine::instance()->pushState(new BeatEmUpState());
+	GameStateMachine::instance()->pushState(new PauseState());
 }
 
 Game::~Game(){ // destructora
